e8b7a5a46f91028f7e57859ea36c159374e3022d
aqwuarvitha <aqwuarvitha@gmail.com>
2020-03-18 14:25:49 +0700

    commit

---
+++ application/controllers/Test_api.php
@@ -0,0 +1,183 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test_api extends CI_Controller {
+
+	function index()
+	{
+		$this->load->view('api_view');
+	}
+
+	function action()
+	{
+		if($this->input->post('data_action'))
+		{
+			$data_action = $this->input->post('data_action');
+
+			if($data_action == "Delete")
+			{
+				$api_url = "http://localhost/FrameworkWeb/crud_api/api/delete";
+
+				$form_data = array(
+					'nim'		=>	$this->input->post('nim')
+				);
+
+				$client = curl_init($api_url);
+
+				curl_setopt($client, CURLOPT_POST, true);
+
+				curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+
+				curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+
+				$response = curl_exec($client);
+
+				curl_close($client);
+
+				echo $response;
+
+
+
+
+			}
+
+			if($data_action == "Edit")
+			{
+				$api_url = "http://localhost/FrameworkWeb/crud_api/api/update";
+
+				$form_data = array(
+					'nama'		=>	$this->input->post('nama'),
+					'nim'			=>	$this->input->post('nim')
+					// 'id'				=>	$this->input->post('user_id')
+				);
+
+				$client = curl_init($api_url);
+
+				curl_setopt($client, CURLOPT_POST, true);
+
+				curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+
+				curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+
+				$response = curl_exec($client);
+
+				curl_close($client);
+
+				echo $response;
+
+
+
+
+
+
+
+			}
+
+			if($data_action == "fetch_single")
+			{
+				$api_url = "http://localhost/FrameworkWeb/crud_api/api/fetch_single";
+
+				$form_data = array(
+					'nim'		=>	$this->input->post('nim')
+				);
+
+				$client = curl_init($api_url);
+
+				curl_setopt($client, CURLOPT_POST, true);
+
+				curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+
+				curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+
+				$response = curl_exec($client);
+
+				curl_close($client);
+
+				echo $response;
+
+
+
+
+
+
+			}
+
+			if($data_action == "Insert")
+			{
+				$api_url = "http://localhost/FrameworkWeb/crud_api/api/insert";
+			
+
+				$form_data = array(
+					'nama'		=>	$this->input->post('nama'),
+					'nim'			=>	$this->input->post('nim')
+				);
+
+				$client = curl_init($api_url);
+
+				curl_setopt($client, CURLOPT_POST, true);
+
+				curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+
+				curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+
+				$response = curl_exec($client);
+
+				curl_close($client);
+
+				echo $response;
+
+
+			}
+
+
+
+
+
+			if($data_action == "fetch_all")
+			{
+				$api_url = "http://localhost/FrameworkWeb/crud_api/api";
+
+				$client = curl_init($api_url);
+
+				curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+
+				$response = curl_exec($client);
+
+				curl_close($client);
+
+				$result = json_decode($response);
+
+				$output = '';
+
+				if(count($result) > 0)
+				{
+					foreach($result as $row)
+					{
+						$output .= '
+						<tr>
+							<td>'.$row->nim.'</td>
+							<td>'.$row->nama.'</td>
+							<td><butto type="button" name="edit" class="btn btn-warning  edit" id="'.$row->nim.'"> <i class="material-icons" style="font-size:15px">edit</i></button></td>
+							<td><button type="button" name="delete" class="btn btn-danger  delete" id="'.$row->nim.'"><i class="material-icons" style="font-size:15px">delete</i></button></td>
+						</tr>
+
+						';
+					}
+				}
+				else
+				{
+					$output .= '
+					<tr>
+						<td colspan="4" align="center">No Data Found</td>
+					</tr>
+					';
+				}
+
+				echo $output;
+			}
+		}
+	}
+	
+}
+
+?>
